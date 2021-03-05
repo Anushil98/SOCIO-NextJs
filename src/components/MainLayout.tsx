4f1858311165fa6f4f125b50c769466379b72bd1
Anushil98 <anushilghoshdastidar@gmail.com>
2021-03-06 03:10:23 +0530

    navbar

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,13 +1,55 @@
 import Link from 'next/link';
 export const MainLayout = (props) => {
-    return (
-        <div className="MainLayout">
-            <div className="NavBar"><div>SkillIt</div><div><Link href="/HomePage"><a>Home</a></Link></div></div>
-            <div className="ContentArea">
-                <div className="leftSideBar">{props.leftSideBar}</div>
-                <div className="Middle" id="MidArea">{props.Middle}</div>
-                <div className="rightSideBar">{props.rightSideBar}</div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="MainLayout">
+			<div className="NavBar">
+				<div className="NavBar-left">
+					<div>
+						<div>SOCIO</div>
+					</div>
+				</div>
+				<div className="NavBar-Right">
+					<div className="NavBar-Buttons">
+						<div className="NavBar-Buttons-Sub">
+							<div>
+								<Link href="/error">
+									<a className="NavBarButton">College</a>
+								</Link>
+								<Link href="/error">
+									<a className="NavBarButton Mobile">C</a>
+								</Link>
+							</div>
+						</div>
+						<div className="NavBar-Buttons-Sub">
+							<div>
+								<Link href="/error">
+									<a className="NavBarButton">Profile</a>
+								</Link>
+								<Link href="/error">
+									<a className="NavBarButton Mobile">P</a>
+								</Link>
+							</div>
+						</div>
+						<div className="NavBar-Buttons-Sub">
+							<div>
+								<Link href="/error">
+									<a className="NavBarButton">Settings</a>
+								</Link>
+								<Link href="/error">
+									<a className="NavBarButton Mobile">S</a>
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="ContentArea">
+				<div className="leftSideBar">{props.leftSideBar}</div>
+				<div className="Middle" id="MidArea">
+					{props.Middle}
+				</div>
+				<div className="rightSideBar">{props.rightSideBar}</div>
+			</div>
+		</div>
+	);
+};
--- src/components/SideCards.tsx
+++ src/components/SideCards.tsx
@@ -1,5 +1,3 @@
 export default function SideCard(props) {
-    return (
-        <div className="SideCard">{props.children}</div>
-    )
+	return <div className="SideCard">{props.children}</div>;
 }

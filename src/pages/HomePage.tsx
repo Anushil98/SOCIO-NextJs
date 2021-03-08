c738393b7ae390f37743b7c45a45b97748cd2aed
Anushil98 <anushilghoshdastidar@gmail.com>
2021-03-08 18:16:17 +0530

    checkdeployment

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,22 @@
-import Router from "next/router"
-import { useEffect } from "react"
-import { MainLayout } from "../components/MainLayout"
-import checkAuth from "../helpers/checkAuth"
-import logout from "../helpers/logout"
+import Router from 'next/router';
+import { useEffect } from 'react';
+import { MainLayout } from '../components/MainLayout';
+import checkAuth from '../helpers/checkAuth';
+import logout from '../helpers/logout';
 
 export default function HomePage() {
-    useEffect(() => {
-        const auth = checkAuth()
-        console.log(auth);
-        if (auth === false) {
-            logout()
-            Router.push("/");
-        }
-    })
-    return <MainLayout><div>Welcome</div></MainLayout>
+	useEffect(() => {
+		Router.push('/');
+		const auth = checkAuth();
+		console.log(auth);
+		if (auth === false) {
+			logout();
+			Router.push('/');
+		}
+	});
+	return (
+		<MainLayout>
+			<div>Welcome</div>
+		</MainLayout>
+	);
 }

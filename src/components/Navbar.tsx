94be44a66e69247f8370987091e2331b86c8047b
Anushil98 <anushilghoshdastidar@gmail.com>
2021-04-07 13:09:36 +0530

    NavBarTop_Design_Handover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,69 +1,14 @@
-import AddIcon from '@material-ui/icons/Add';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import PersonIcon from '@material-ui/icons/Person';
-import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 
 export default function Navbar(props) {
 	return (
 		<div className="NavBar">
-			<div className="NavBar-left">
-				<div>SOCIO</div>
-			</div>
-			<div className="NavBar-Right">
-				<div className="NavBar-Buttons-Sub">
-					<div>
-						<div>
-							<div className="NavBarButton">
-								<PeopleAltIcon fontSize="large" />
-								<span>Groups</span>
-							</div>
-
-							<div className="NavBarButton Mobile">
-								<PeopleAltIcon fontSize="large" />
-							</div>
-						</div>
-					</div>
+			<div className="NavBarTop">
+				<div className="NavBarTop-Left">
+					<img src="/FullLogoVertical.svg" alt="Logo" className="Logo" />
 				</div>
-				<div className="NavBar-Buttons-Sub">
-					<div>
-						<div className="NavBarButton">
-							<SearchIcon fontSize="large" />
-							<span>Search</span>
-						</div>
-
-						<div className="NavBarButton Mobile">
-							<SearchIcon fontSize="large" />
-						</div>
-					</div>
-				</div>
-				<div id="createPost-Button" onClick={() => props.showCreatePostModal()}>
-					<AddIcon fontSize="large" />
-				</div>
-				<div className="NavBar-Buttons-Sub">
-					<div>
-						<div className="NavBarButton">
-							<NotificationsIcon fontSize="large" />
-							<span>Notifications</span>
-						</div>
-
-						<div className="NavBarButton Mobile">
-							<NotificationsIcon fontSize="large" />
-						</div>
-					</div>
-				</div>
-				<div className="NavBar-Buttons-Sub">
-					<div>
-						<div className="NavBarButton">
-							<PersonIcon fontSize="large" />
-							<span>Profile</span>
-						</div>
-
-						<div className="NavBarButton Mobile">
-							<PersonIcon fontSize="large" />
-						</div>
-					</div>
+				<div className="NavBarTop-Right">
+					<img src="/SearchIcon.svg" alt="" className="SearchButton" />
 				</div>
 			</div>
 		</div>
